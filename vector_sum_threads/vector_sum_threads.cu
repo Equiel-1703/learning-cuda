f0d847ced1164f4ab6173ea415f9829c6ba4e739
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2025-04-10 21:37:26 -0300

    Created vector sum implementaton using GPU threads

---
+++ vector_sum_threads/vector_sum_threads.cu
@@ -0,0 +1,97 @@
+#include <iostream>
+
+#include "../common/include/Tools.hpp"
+#include "../common/include/CPUTimer.hpp"
+
+#define VEC_SIZE 600'000
+#define NUM_ELEMENTS_PREVIEW 10
+
+#define THREADS_PER_BLOCK 512
+
+__global__ void vec_sum(int *vec_1, int *vec_2, int *vec_3)
+{
+    int vec_index = threadIdx.x;
+
+    if (vec_index < VEC_SIZE)
+    {
+        vec_3[vec_index] = vec_1[vec_index] + vec_2[vec_index];
+    }
+}
+
+int main()
+{
+    // Criando vetores na CPU e medidor de tempo da GPU
+    int a[VEC_SIZE], b[VEC_SIZE], c[VEC_SIZE];
+    CPUTimer timer;
+
+    // Preenchendo vetores
+    for (int i = 0; i < VEC_SIZE; i++)
+    {
+        a[i] = i;
+        b[i] = 2 * i;
+    }
+
+    // Alocando vetores na GPU
+    int *vec_1, *vec_2, *vec_3;
+
+    CHECK_ERROR(cudaMalloc((void **)&vec_1, sizeof(int) * VEC_SIZE));
+    CHECK_ERROR(cudaMalloc((void **)&vec_2, sizeof(int) * VEC_SIZE));
+    CHECK_ERROR(cudaMalloc((void **)&vec_3, sizeof(int) * VEC_SIZE));
+
+    // Copiando vetores a e b para GPU
+    CHECK_ERROR(cudaMemcpy(vec_1, a, sizeof(a), cudaMemcpyKind::cudaMemcpyHostToDevice));
+    CHECK_ERROR(cudaMemcpy(vec_2, b, sizeof(b), cudaMemcpyKind::cudaMemcpyHostToDevice));
+
+    const int num_blocks = (VEC_SIZE + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK;
+
+    // Iniciando timer
+    timer.start_timer();
+
+    // Chamando kernel para realizar a computação. Estou lançando um bloco para cada elemento do vetor.
+    vec_sum<<<num_blocks, THREADS_PER_BLOCK>>>(vec_1, vec_2, vec_3);
+
+    CHECK_ERROR(cudaDeviceSynchronize());
+
+    // Parando timer
+    timer.stop_timer();
+
+    // Copiando o resultado pra CPU
+    CHECK_ERROR(cudaMemcpy(c, vec_3, sizeof(c), cudaMemcpyKind::cudaMemcpyDeviceToHost));
+
+    std::cout << "A: ";
+    for (int i = 0; i < NUM_ELEMENTS_PREVIEW; i++)
+    {
+        std::cout << a[i] << " ";
+    }
+    std::cout << "..." << std::endl;
+
+    std::cout << "B: ";
+    for (int i = 0; i < NUM_ELEMENTS_PREVIEW; i++)
+    {
+        std::cout << b[i] << " ";
+    }
+    std::cout << "..." << std::endl;
+
+    std::cout << "C: ";
+    for (int i = 0; i < NUM_ELEMENTS_PREVIEW; i++)
+    {
+        std::cout << c[i] << " ";
+    }
+    std::cout << "..." << std::endl;
+
+    std::cout << "Número de blocos: " << num_blocks
+              << " | threads/bloco: " << THREADS_PER_BLOCK
+              << std::endl;
+    std::cout << "Número de elementos: " << VEC_SIZE << std::endl;
+    std::cout << "Tempo de execução: "
+              << timer.get_elapsed_time_ns() << " ns / "
+              << timer.get_elapsed_time_ms() << " ms"
+              << std::endl;
+
+    // Liberando memória
+    CHECK_ERROR(cudaFree(vec_1));
+    CHECK_ERROR(cudaFree(vec_2));
+    CHECK_ERROR(cudaFree(vec_3));
+
+    return 0;
+}
